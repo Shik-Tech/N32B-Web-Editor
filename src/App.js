32f1af1363ea2648f3dc407ff9b4dd0e549526be
Shik <shiko.c@gmail.com>
2023-05-27 11:31:44 +0300

    Move header to a module

--- src/App.js
+++ src/App.js
@@ -4,27 +4,19 @@ import { WebMidi } from "webmidi";
 import { findIndex } from 'lodash';
 import { Container } from '@mui/system';
 import {
-  AppBar,
   Box,
-  Button,
   Divider,
   Stack,
-  Toolbar,
   Typography
 } from '@mui/material';
-import UploadFileRoundedIcon from '@mui/icons-material/UploadFileRounded';
-import SimCardDownloadRoundedIcon from '@mui/icons-material/SimCardDownloadRounded';
 
 import './App.css';
-import logo from './components/images/shik-logo-small.png';
 import {
   N32B,
   Editor,
   SysExEditor,
-  UpdateDevice,
   ConnectDevice,
   Version,
-  SyncDevice,
   SystemMessages,
   ThruMode,
   ThruOptions,
@@ -60,6 +52,7 @@ import {
 import defaultPresets from './presetTemplates/default';
 import sysExPreset from './presetTemplates/default/sysEx.json'
 import { byteString } from './utils';
+import AppHeader from './components/AppHeader/AppHeader';
 
 function App() {
   const {
@@ -336,96 +329,22 @@ function App() {
         message={systemMessage}
       />
       <Box>
-        <AppBar position="static" >
-          <Toolbar variant="dense">
-            <Stack direction="row" spacing={2} sx={{ flexGrow: 1 }}>
-              <Stack
-                direction="row"
-                spacing={2}
-                divider={<Divider orientation="vertical" light />}
-                sx={{ flexGrow: 1 }}
-              >
-                <Box
-                  component="img"
-                  alt="SHIK logo"
-                  src={logo}
-                  sx={{
-                    height: 20,
-                    pt: 1
-                  }}
-                />
-                <Typography sx={{ pt: 1 }} variant="body2" component="div">
-                  N32B Editor
-                </Typography>
-                {deviceIsConnected && firmwareVersion &&
-                  <Typography sx={{ pt: 1 }} variant="body2" component="div">
-                    {midiDeviceName} < Typography variant="caption" sx={{ color: "#808080" }} >(v{firmwareVersion.join('.')})</Typography>
-                    {firmwareVersion[0] > 29 &&
-                      " - SysEx"
-                    }
-                  </Typography>
-                }
-              </Stack>
+        <AppHeader
+          firmwareVersion={firmwareVersion}
+          deviceIsConnected={deviceIsConnected}
+          midiOutput={midiOutput}
+          midiDeviceName={midiDeviceName}
+          isSyncing={isSyncing}
+          currentPreset={currentPreset}
+          fileInput={fileInput}
 
-              {deviceIsConnected && !firmwareVersion &&
-                <Button
-                  onClick={handleFirmwareUpdate}
-                  color="error"
-                >
-                  Firmware Update
-                </Button>
-              }
-
-              {deviceIsConnected && firmwareVersion && currentPreset &&
-                <Stack
-                  direction="row"
-                  spacing={2}
-                >
-                  <Button
-                    fullWidth
-                    variant="outlined"
-                    endIcon={<UploadFileRoundedIcon />}
-                    onClick={handleFileInputClick}
-                  >
-                    Load
-                    <input
-                      hidden
-                      type="file"
-                      ref={fileInput}
-                      onChange={handleLoadPreset}
-                    />
-                  </Button>
-                  <Button
-                    fullWidth
-                    variant="outlined"
-                    color="success"
-                    endIcon={<SimCardDownloadRoundedIcon />}
-                    onClick={handleSavePreset}
-                  >
-                    Save
-                  </Button>
-
-                  <UpdateDevice
-                    firmwareVersion={firmwareVersion}
-                    currentPreset={currentPreset}
-                    midiOutput={midiOutput}
-                    currentDevicePresetIndex={currentPreset.presetID}
-                    handlePresetUpdate={handlePresetUpdate}
-                  />
-
-                  <SyncDevice
-                    isSyncing={isSyncing}
-                    firmwareVersion={firmwareVersion}
-                    currentPreset={currentPreset}
-                    currentDevicePresetIndex={currentPreset.presetID}
-                    handlePresetUpdate={handlePresetUpdate}
-                    handleLoadFromDevice={handleLoadFromDevice}
-                  />
-                </Stack>
-              }
-            </Stack>
-          </Toolbar>
-        </AppBar>
+          handleFirmwareUpdate={handleFirmwareUpdate}
+          handleFileInputClick={handleFileInputClick}
+          handleLoadPreset={handleLoadPreset}
+          handleSavePreset={handleSavePreset}
+          handlePresetUpdate={handlePresetUpdate}
+          handleLoadFromDevice={handleLoadFromDevice}
+        />
 
         {!deviceIsConnected &&
           <ConnectDevice />
---
+++ src/components/AppHeader/AppHeader.js
@@ -0,0 +1,132 @@
+import React from 'react';
+
+import {
+    AppBar,
+    Box,
+    Button,
+    Divider,
+    Stack,
+    Toolbar,
+    Typography
+} from '@mui/material';
+import UploadFileRoundedIcon from '@mui/icons-material/UploadFileRounded';
+import SimCardDownloadRoundedIcon from '@mui/icons-material/SimCardDownloadRounded';
+
+import '../../App.css';
+import logo from '../images/shik-logo-small.png';
+import {
+    UpdateDevice,
+    SyncDevice,
+} from '../';
+
+function AppHeader({
+    firmwareVersion,
+    deviceIsConnected,
+    midiOutput,
+    midiDeviceName,
+    isSyncing,
+    currentPreset,
+    fileInput,
+
+    handleFirmwareUpdate,
+    handleFileInputClick,
+    handleLoadPreset,
+    handleSavePreset,
+    handlePresetUpdate,
+    handleLoadFromDevice
+}) {
+    return (
+        <AppBar position="static" >
+            <Toolbar variant="dense">
+                <Stack direction="row" spacing={2} sx={{ flexGrow: 1 }}>
+                    <Stack
+                        direction="row"
+                        spacing={2}
+                        divider={<Divider orientation="vertical" light />}
+                        sx={{ flexGrow: 1 }}
+                    >
+                        <Box
+                            component="img"
+                            alt="SHIK logo"
+                            src={logo}
+                            sx={{
+                                height: 20,
+                                pt: 1
+                            }}
+                        />
+                        <Typography sx={{ pt: 1 }} variant="body2" component="div">
+                            N32B Editor
+                        </Typography>
+                        {deviceIsConnected && firmwareVersion &&
+                            <Typography sx={{ pt: 1 }} variant="body2" component="div">
+                                {midiDeviceName} < Typography variant="caption" sx={{ color: "#808080" }} >(v{firmwareVersion.join('.')})</Typography>
+                                {firmwareVersion[0] > 29 &&
+                                    " - SysEx"
+                                }
+                            </Typography>
+                        }
+                    </Stack>
+
+                    {deviceIsConnected && !firmwareVersion &&
+                        <Button
+                            onClick={handleFirmwareUpdate}
+                            color="error"
+                        >
+                            Firmware Update
+                        </Button>
+                    }
+
+                    {deviceIsConnected && firmwareVersion && currentPreset &&
+                        <Stack
+                            direction="row"
+                            spacing={2}
+                        >
+                            <Button
+                                fullWidth
+                                variant="outlined"
+                                endIcon={<UploadFileRoundedIcon />}
+                                onClick={handleFileInputClick}
+                            >
+                                Load
+                                <input
+                                    hidden
+                                    type="file"
+                                    ref={fileInput}
+                                    onChange={handleLoadPreset}
+                                />
+                            </Button>
+                            <Button
+                                fullWidth
+                                variant="outlined"
+                                color="success"
+                                endIcon={<SimCardDownloadRoundedIcon />}
+                                onClick={handleSavePreset}
+                            >
+                                Save
+                            </Button>
+
+                            <UpdateDevice
+                                firmwareVersion={firmwareVersion}
+                                currentPreset={currentPreset}
+                                midiOutput={midiOutput}
+                                currentDevicePresetIndex={currentPreset.presetID}
+                                handlePresetUpdate={handlePresetUpdate}
+                            />
+
+                            <SyncDevice
+                                isSyncing={isSyncing}
+                                firmwareVersion={firmwareVersion}
+                                currentPreset={currentPreset}
+                                currentDevicePresetIndex={currentPreset.presetID}
+                                handlePresetUpdate={handlePresetUpdate}
+                                handleLoadFromDevice={handleLoadFromDevice}
+                            />
+                        </Stack>
+                    }
+                </Stack>
+            </Toolbar>
+        </AppBar>
+    )
+}
+
+export default AppHeader;
